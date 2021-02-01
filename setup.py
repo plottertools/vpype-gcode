2429f53c49defe2ff28cd7e52dec8ff6fc501abb
tatarize <tatarize@gmail.com>
2021-01-31 21:04:33 -0800

    linecollection -> line, readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="vpype-gcode",
-    version="0.7.0",
+    version="0.7.1",
     description="vpype gcode plugin",
     long_description=readme,
     long_description_content_type="text/markdown",
