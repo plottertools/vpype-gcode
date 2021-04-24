260984840ddb1d5a14e1b43a6877197fada72b84
Tatarize <tatarize@gmail.com>
2021-04-24 14:18:57 -0700

    Version Bump 0.8.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="vpype-gcode",
-    version="0.8.1",
+    version="0.8.2",
     description="vpype gcode plugin",
     long_description=readme,
     long_description_content_type="text/markdown",
