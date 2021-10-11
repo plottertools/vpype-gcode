63c381a90ec6a984b5fdccb88e2b427bd5f13e70
Tatarize <tatarize@gmail.com>
2021-10-11 06:36:11 -0700

    Additions to classifiers

--- setup.py
+++ setup.py
@@ -16,6 +16,11 @@ setup(
     classifiers=(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Environment :: Console",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Graphics",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
