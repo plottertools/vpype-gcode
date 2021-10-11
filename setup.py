c27c0b8800d1ba0378ad78cc7145b3f7fc4cf097
Tatarize <tatarize@gmail.com>
2021-10-11 06:32:59 -0700

    Correct Default Profiles. Inform User of Change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="vpype-gcode",
-    version="0.9.2",
+    version="0.10.0",
     description="vpype gcode plugin",
     long_description=readme,
     long_description_content_type="text/markdown",
