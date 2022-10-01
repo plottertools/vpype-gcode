b8e04d544529a5d24873d02a430b5b9e7623aae1
Tobin Yehle <tobinyehle@gmail.com>
2022-10-01 00:31:51 -0600

    Update documentation

--- vpype_gcode/gwrite.py
+++ vpype_gcode/gwrite.py
@@ -18,6 +18,7 @@ def invert_axis(document: vp.Document, invert_x: bool, invert_y: bool):
     """Inverts none, one or both axis of the document.
     This applies a relative scale operation with factors of 1 or -1
     on the two axis to all layers. The inversion happens relative to
+    the center of the page, if the page size has been set, otherwise to
     the center of the bounds.
     """
 
